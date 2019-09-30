f7d30ef512bd8cf3157993876d32d8d5650939c2
Rien van Velzen <rienvanvelzen@gmail.com>
2019-09-30 21:51:04 +0200

    Allow scrolling on the detail page

--- lib/pages/GameDetails.dart
+++ lib/pages/GameDetails.dart
@@ -12,32 +12,34 @@ class GameDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(_game.getName())),
-      body: Column(
-        children: <Widget>[
-          Image(
-            fit: BoxFit.fitWidth,
-            image: NetworkImage(_game.getCoverUrl()),
-          ),
-          Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    _game.getName(),
-                    style: TextStyle(fontSize: 24)
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Image(
+              fit: BoxFit.fitWidth,
+              image: NetworkImage(_game.getCoverUrl()),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      _game.getName(),
+                      style: TextStyle(fontSize: 24)
+                    ),
                   ),
-                ),
-                Text(
-                  _game.getSummary(),
-                  style: TextStyle(fontSize: 16)
-                ),
-              ],
-            )
-          ),
-        ],
+                  Text(
+                    _game.getSummary(),
+                    style: TextStyle(fontSize: 16)
+                  ),
+                ],
+              )
+            ),
+          ],
+        ),
       ),
     );
   }
